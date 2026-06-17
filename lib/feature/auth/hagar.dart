6f38b1ae424f53c5951183285f1ab4b5989339ed
Hagar Elganzory <hagarelganzory51@gmail.com>
2026-06-17 22:14:16 +0300

    first

---
+++ lib/feature/auth/hagar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Hagar extends StatelessWidget {
+  const Hagar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
