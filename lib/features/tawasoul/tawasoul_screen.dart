9333ee7cec91bafb807510350b0f9e0cd317b961
Tawasoulmohamedkhair <tawasoulmohamedkhair@gmail.com>
2026-06-17 22:29:08 +0400

    make tawasoul branch and Add Feature of Tawasoul

---
+++ lib/features/tawasoul/tawasoul_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TawasoulScreen extends StatelessWidget {
+  const TawasoulScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('tawasoul Screen'),),
+    );
+  }
+}
