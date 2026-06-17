bc0e6ad1bff95432d2c189a8d20def2347597ab8
Essam <essam.ibrahim.0062@gmail.com>
2026-06-17 21:30:10 +0300

    add EssamIbrahimScreene widget with basic UI structure

---
+++ lib/features/essam_ibrahim/essam_ibrahim_screene.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EssamIbrahimScreene extends StatelessWidget {
+  const EssamIbrahimScreene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Essam Ibrahim Screene'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Essam Ibrahim Screene!'),
+      ),
+    );
+  }
+}
