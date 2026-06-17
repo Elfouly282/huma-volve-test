3a3ea072beb6c463bd9cf150d3bdef8b3e017fa7
maream khaled ahmed <mareamkmousbah@gmail.com>
2026-06-17 21:41:21 +0300

    add mariam screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test1/features/home/mariam_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MariamScreen(),
     );
   }
 }
