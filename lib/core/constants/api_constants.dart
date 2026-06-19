944cb120a9a624220ae20b4cd4ce1ba21b68cfc1
Nadia khaled <d703403@gmail.com>
2026-06-19 20:34:35 +0300

    add api_constants and failure handling classes with dio integration

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,9 @@
+class ApiConstants {
+  ApiConstants._();
+  static final ApiConstants _instance = ApiConstants._();
+  factory ApiConstants() => _instance;
+
+  static final String baseUrl = "https://grocery.newcinderella.online/api";
+ // static final String  login= '/auth/login';
+  static final String register = '/auth/register';
+}
